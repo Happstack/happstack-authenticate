0f2cbd6a471aa2c13b0118bd8e5c76eb6c9b3570
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-07 22:52:14 -0500

    added default.nix and shell.nix

---
+++ default.nix
@@ -0,0 +1,26 @@
+{ mkDerivation, acid-state, aeson, authenticate, base
+, base64-bytestring, boomerang, bytestring, containers
+, data-default, filepath, happstack-hsp, happstack-jmacro
+, happstack-server, hsp, hsx-jmacro, hsx2hs, http-conduit
+, http-types, ixset-typed, jmacro, jwt, lens, mime-mail, mtl
+, pwstore-purehaskell, random, safecopy, shakespeare, stdenv, text
+, time, unordered-containers, web-routes, web-routes-boomerang
+, web-routes-happstack, web-routes-hsp, web-routes-th
+}:
+mkDerivation {
+  pname = "happstack-authenticate";
+  version = "2.1.2";
+  src = ./.;
+  buildDepends = [
+    acid-state aeson authenticate base base64-bytestring boomerang
+    bytestring containers data-default filepath happstack-hsp
+    happstack-jmacro happstack-server hsp hsx-jmacro hsx2hs
+    http-conduit http-types ixset-typed jmacro jwt lens mime-mail mtl
+    pwstore-purehaskell random safecopy shakespeare text time
+    unordered-containers web-routes web-routes-boomerang
+    web-routes-happstack web-routes-hsp web-routes-th
+  ];
+  homepage = "http://www.happstack.com/";
+  description = "Happstack Authentication Library";
+  license = stdenv.lib.licenses.bsd3;
+}
